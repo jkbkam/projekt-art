cc0af50bef8c7e780681a42931dd99b5449277b9
jkbkam <jkb.kam@gmail.com>
2019-02-14 22:57:08 +0100

    desktop gotwy, dorobic mobile

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
+$(document).on("click", 'a[href^="#"]', function(event) {
+  event.preventDefault();
 
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top - 150
+    },
+    500
+  );
+});
 
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+var oferts = document.querySelectorAll(".ofert-img"),
+aboutImg = document.querySelectorAll(".about-img")[0];
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
+console.log(aboutImg)
+
+window.addEventListener("scroll", function() {
+var bottomWindow = (window.innerHeight*0.7) + window.scrollY,
+aboutPosition = aboutImg.offsetTop - aboutImg.clientHeight/2;
+
+if (bottomWindow > aboutPosition) {
+     aboutImg.classList.add("about-show");
+  }
+
+  oferts.forEach(function(ofert) {
+    var elementPosition = ofert.offsetTop + ofert.clientHeight;
+    if (bottomWindow > elementPosition) {
+      ofert.classList.add("ofert-show");
+    }
+  });
 });
